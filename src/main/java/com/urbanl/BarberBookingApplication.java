f868a51745bf7aee4e63273c784d8b62030cfa71
lukaszurban <urbanlukasz94@gmail.com>
2020-11-05 18:41:18 +0100

    Initial commit

---
+++ src/main/java/com/urbanl/BarberBookingApplication.java
@@ -0,0 +1,12 @@
+package com.urbanl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarberBookingApplication.class, args);
+    }
+}
